1da1d5306fd266f61cc58d50f97ee5b453be6712
Ayush Gupta <agupta@proteus.com>
2020-09-09 17:32:13 -0400

    Initial Commit

---
+++ AmplifyAWS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AmplifyAWS
+//
+//  Created by Ayush Gupta on 9/9/20.
+//  Copyright © 2020 Ayush Gupta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
